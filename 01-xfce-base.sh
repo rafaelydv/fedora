2c57176d2785e168d96456b320e91eaaed37f1db
Rafael Y. De Vidis <rafael.ydv@gmail.com>
2020-06-25 12:00:49 -0400

    Script de instalação do Xfce.

---
+++ 01-xfce-base.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+## Pacotes base do Xfce
+dnf istall @base-x \
+	   xfwm4 \
+	   xfce4-panel \
+	   xfce4-session \
+	   xfce4-settings \
+	   xfce4-notifyd \
+	   xfce4-datetime-plugin \
+	   xfce4-genmon-plugin \
+	   xfce4-whiskermenu-plugin \
+	   xfce4-taskmanager \
+	   xfce4-screenshooter\
+	   xfce4-pulseaudio-plugin \
+	   xfce4-power-manager \
+	   xfce4-notes-plugin \
+	   terminator \
+	   xdg-user-dirs \
+	   pcmanfm \
+	   gvfs-fuse \
+	   gvfs-smb \
+	   lightdm-gtk-greeter-settings \
+	   p7zip \
+	   xarchiver
