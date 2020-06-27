0d4b39d1e38d033290e4085c6f2e6a976b79f03e
Rafael Y. De Vidis <rafael.ydv@gmail.com>
2020-06-27 03:30:41 -0400

    Pacotes utilitários.

---
+++ utilitarios.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+dnf install gparted \
+	    libreoffice-writer \
+	    libreoffice-calc \
+	    libreoffice-impress \
+	    libreoffice-l10n-pt-br \
+	    kolourpaint \
+	    keepass2 \
+	    zip \
+	    unzip \
+	    vlc
