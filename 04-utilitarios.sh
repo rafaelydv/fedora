296c4157dc8f114c72c99fe7b26a4488f3172e4b
Rafael Y. De Vidis <rafael.ydv@gmail.com>
2020-07-02 12:55:21 -0400

    Corrigido nome do pacote de idiomas do Libreoffice.

--- 04-utilitarios.sh
+++ 04-utilitarios.sh
@@ -4,7 +4,7 @@ dnf install gparted \
 	    libreoffice-writer \
 	    libreoffice-calc \
 	    libreoffice-impress \
-	    libreoffice-pt-BR \
+	    libreoffice-langpack-pt-BR \
 	    kolourpaint \
 	    keepass2 \
 	    zip \
