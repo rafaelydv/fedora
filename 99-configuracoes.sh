ca4417b1376dc937253600a4b0043173d0e0c795
Rafael Y. De Vidis <rafael.ydv@gmail.com>
2020-07-02 13:09:54 -0400

    Ajustado carregamento das configurações do desktop.

--- 99-configuracoes.sh
+++ 99-configuracoes.sh
@@ -3,5 +3,4 @@
 dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 ## Habilitar carregamento da interface gráfica no boot
-systemctl isolate graphical.target
 systemctl set-default graphical.target
