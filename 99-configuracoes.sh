d1b82e065da15312258240959f974497f50f5901
Rafael Y. De Vidis <rafael.ydv@gmail.com>
2020-07-02 13:05:35 -0400

    Renomeado para contemplar as configurações do desktop.

--- 99-configuracoes.sh
+++ 99-configuracoes.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 ## Habilitar repositório RPMFusion Fedora31
 dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+## Habilitar carregamento da interface gráfica no boot
+systemctl isolate graphical.target
+systemctl set-default graphical.target
